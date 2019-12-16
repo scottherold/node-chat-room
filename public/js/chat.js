904e01d7ecffe75d0500823ed691808a829b7505
Scott Herold <sherold@mail.usf.edu>
2019-12-16 05:46:25 -0500

    Socket IO Setup

---
+++ public/js/chat.js
@@ -0,0 +1,5 @@
+// *** Client-side JS *** //
+
+// *** WEBSOCKETING *** //
+// ** Connection ** //
+io();
--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@
 // *** MODULES *** //
 // ** native Modules ** //
 const path = require('path')
+const http = require('http') // <-- needed to split websockets from traditional http requests
 
 // ** npm Modules ** //
 const express = require('express');
+const socketio = require('socket.io');
 
 // *** SERVER CREATION *** //
 const app = express();
+const server = http.createServer(app); // <-- split out from app
 const port = process.env.PORT;
+
+// *** WEBSOCKETING CREATION *** //
+const io = socketio(server);
 
 // *** EXPRESS CONFIG *** //
 // ** Paths ** //
 const publicDirectoryPath = path.join(__dirname, '../public');
 
-// ** Config ** //
+// ** Settings ** //
 app.use(express.static(publicDirectoryPath));
 
 // *** SERVER ROUTING *** //
@@ -23,7 +29,14 @@ app.get('', (req, res) => {
     res.render('index.html');
 });
 
+// *** SOCKETING *** //
+// on connect
+io.on('connection', () => {
+    console.log('New WebSocket connection');
+});
+
 // *** SERVER INSTANTIATION *** //
-app.listen(port, () => {
+// the HTTP server now listens, leaving the express app open for websocketing
+server.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
